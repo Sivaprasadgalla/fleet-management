57ba5cdf60f8e6cedfdbbbf11d7de3a49efc41f9
prasad-galla-inteocean <prasad.galla.inteocean@gmail.com>
2026-08-16 19:05:08 +0530

    retuen all drivers and vehicles data based on user role

--- backend/src/controllers/driver.controller.js
+++ backend/src/controllers/driver.controller.js
@@ -1,78 +1,177 @@
-const Driver =require('../models/driver.model')
-const {validationResult} =require('express-validator')
+const Driver = require("../models/driver.model");
+const User = require("../models/user.model");
+const { validationResult } = require("express-validator");
 
-const createDriver =async(req,res)=>{
-    try {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({message:"All Fields Are Required",
-                errors:errors.array().map((err)=>err.msg).join(', ')
-            })
-        }
-        const {userId,firstName,lastName,phoneNumber,email,licenseNumber,licenseExpiry,status} =req.body;
-        const driverExits = await Driver.findOne({user:userId,email})
-        if(driverExits){
-            return res.status(400).json({message:"Driver Already Exist!!"})
-        }
-        const newDriver = await Driver.create({user:userId,firstName,lastName,phoneNumber,email,licenseNumber,licenseExpiry,status})
-        res.status(201).json({message:"Driver Created Successfully", data : newDriver})
-
-    } catch (error) {
-        res.status(500).json({message:"Server Error Occured",error:error.message})
+const createDriver = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "All Fields Are Required",
+        errors: errors
+          .array()
+          .map((err) => err.msg)
+          .join(", "),
+      });
     }
-}
-
-const getAllDrivers = async(req,res)=>{
-    try {
-        const {userId} =req.query;
-        const allDrivers = await Driver.find({user:userId})
-        res.status(200).json({message:"All Driver Details",data:allDrivers})
-    } catch (error) {
-         res.status(500).json({message:"Server Error Occured",error:error.message})
+    const {
+      userId,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      licenseNumber,
+      licenseExpiry,
+      status,
+    } = req.body;
+    const driverExits = await Driver.findOne({ user: userId, email });
+    if (driverExits) {
+      return res.status(400).json({ message: "Driver Already Exist!!" });
     }
-}
+    const newDriver = await Driver.create({
+      user: userId,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      licenseNumber,
+      licenseExpiry,
+      status,
+    });
+    res
+      .status(201)
+      .json({ message: "Driver Created Successfully", data: newDriver });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
 
-const getOneDriver =async(req,res)=>{
-    try {
-        const {id} = req.params;
-        const {userId} =req.query;
-        const getOne = await Driver.findOne({_id:id,user:userId})
-        res.status(200).json({message:"One Driver Details",data:getOne})
-    } catch (error) {
-        res.status(500).json({message:"Server Error Occured",error:error.message})
+const getAllDrivers = async (req, res) => {
+  try {
+    const { userId } = req.query;
+    if (!userId) {
+      return res.status(400).json({
+        message: "userId is required",
+      });
     }
-}
 
-const updateDriver =async(req,res)=>{
-    try {
-         const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({message:"All Fields Are Required",
-                errors:errors.array().map((err)=>err.msg).join(', ')
-            })
-        }
-       const {id} =req.params;
-    const {userId,firstName,lastName,phoneNumber,email,licenseNumber,licenseExpiry,status} =req.body;
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Admin → get all vehicles
+    if (user.role === "admin") {
+      const drivers = await Driver.find();
+
+      return res.status(200).json({
+        message: "All Drivers details",
+        data: drivers,
+      });
+    }
+
+    // Normal user → get only their vehicles
+    if (user.role === "user") {
+      const drivers = await Driver.find({
+        user: userId,
+      });
+
+      return res.status(200).json({
+        message: "User vehicles details",
+        data: drivers,
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
+
+const getOneDriver = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.query;
+    const getOne = await Driver.findOne({ _id: id, user: userId });
+    res.status(200).json({ message: "One Driver Details", data: getOne });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
+
+const updateDriver = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "All Fields Are Required",
+        errors: errors
+          .array()
+          .map((err) => err.msg)
+          .join(", "),
+      });
+    }
+    const { id } = req.params;
+    const {
+      userId,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      licenseNumber,
+      licenseExpiry,
+      status,
+    } = req.body;
     const update = await Driver.findByIdAndUpdate(
-        id,
-        {user:userId,firstName,lastName,phoneNumber,email,licenseNumber,licenseExpiry,status},
-        {new:true} 
+      id,
+      {
+        user: userId,
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        licenseNumber,
+        licenseExpiry,
+        status,
+      },
+      { new: true },
     );
-    res.status(200).json({message:"Driver Details Updated Successfully",data:update})    
-    } catch (error) {
-        res.status(500).json({message:"Server Error Occured",error:error.message})
-    }
-    
-}
+    res
+      .status(200)
+      .json({ message: "Driver Details Updated Successfully", data: update });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
 
-const deleteDriver = async(req,res)=>{
-    try {
-        const {id} = req.params;
-        const {userId} =req.query;
-        const deleteDriverDetail = await Driver.findByIdAndDelete({_id:id, user:userId})
-        res.status(200).json({message:"Driver Details Deleted Successfully"})
-    } catch (error) {
-        res.status(500).json({message:"Server Error Occured",error:error.message})
-    }
-}
-module.exports ={createDriver,getAllDrivers,getOneDriver,updateDriver,deleteDriver}
+const deleteDriver = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.query;
+    const deleteDriverDetail = await Driver.findByIdAndDelete({
+      _id: id,
+      user: userId,
+    });
+    res.status(200).json({ message: "Driver Details Deleted Successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
+module.exports = {
+  createDriver,
+  getAllDrivers,
+  getOneDriver,
+  updateDriver,
+  deleteDriver,
+};
--- backend/src/controllers/vehicle.controller.js
+++ backend/src/controllers/vehicle.controller.js
@@ -1,83 +1,201 @@
-const Vehicle = require('../models/vehicle.model')
-const {validationResult} =require('express-validator')
+const Vehicle = require("../models/vehicle.model");
+const { validationResult } = require("express-validator");
+const User = require("../models/user.model");
 
-const createVehicle = async(req,res)=>{
-    try {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-           return res.status(400).json({message:"All Fields Are Required",
-            errors:errors.array().map((err)=>err.msg).join(', ')})
-        }
-        const {userId,registerNumber,brand,year,type,fuelType,seatCapacity,InsuranceNumber,InsuranceExpiry,
-            PermitExpiry,status
-        } =req.body;
-       const vehicleExist = await Vehicle.findOne({user:userId,registerNumber})
-       if(vehicleExist){
-         return res.status(400).json({message:"Vehicle Already Exist"})
-       }
-       const newVechile = await Vehicle.create({user:userId,registerNumber,brand,year,type,fuelType,seatCapacity,InsuranceNumber,InsuranceExpiry,
-            PermitExpiry,status})
-        res.status(201).json({message:"Vehicle Registered Successfully", data: newVechile})
-    } catch (error) {
-        res.status(500).json({message:"Server Error Occured",error:error.message})
+const createVehicle = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "All Fields Are Required",
+        errors: errors
+          .array()
+          .map((err) => err.msg)
+          .join(", "),
+      });
     }
-}
+    const {
+      userId,
+      registerNumber,
+      brand,
+      year,
+      type,
+      fuelType,
+      seatCapacity,
+      InsuranceNumber,
+      InsuranceExpiry,
+      PermitExpiry,
+      status,
+    } = req.body;
+    const vehicleExist = await Vehicle.findOne({
+      user: userId,
+      registerNumber,
+    });
+    if (vehicleExist) {
+      return res.status(400).json({ message: "Vehicle Already Exist" });
+    }
+    const newVechile = await Vehicle.create({
+      user: userId,
+      registerNumber,
+      brand,
+      year,
+      type,
+      fuelType,
+      seatCapacity,
+      InsuranceNumber,
+      InsuranceExpiry,
+      PermitExpiry,
+      status,
+    });
+    res
+      .status(201)
+      .json({ message: "Vehicle Registered Successfully", data: newVechile });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
 
-const getAllVehicles =async(req,res)=>{
-try {
-       const {userId} =req.query;
-    const getAll = await Vehicle.find({user:userId})
-    res.status(200).json({message:"All Vehicle Details",data:getAll}) 
-} catch (error) {
-    res.status(500).json({message:"Server Error Occured",error:error.message})
-}
+const getAllVehicles = async (req, res) => {
+  try {
+    const { userId } = req.query;
+    if (!userId) {
+      return res.status(400).json({
+        message: "userId is required",
+      });
+    }
+
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Admin → get all vehicles
+    if (user.role === "admin") {
+      const vehicles = await Vehicle.find();
+
+      return res.status(200).json({
+        message: "All vehicles details",
+        data: vehicles,
+      });
+    }
+
+    // Normal user → get only their vehicles
+    if (user.role === "user") {
+      const vehicles = await Vehicle.find({
+        user: userId,
+      });
+
+      return res.status(200).json({
+        message: "User vehicles details",
+        data: vehicles,
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
+
+const getOneVehicle = async (req, res) => {
+  try {
+    const { userId } = req.query;
+    const { id } = req.params;
+    const getOne = await Vehicle.findOne({ _id: id, user: userId });
+    res.status(200).json({ message: "One Vehicle Details", data: getOne });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
+
+const updateVehicle = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "All Feilds Are Required",
+        errors: errors
+          .array()
+          .map((err) => err.msg)
+          .join(", "),
+      });
+    }
+    const { id } = req.params;
+    const {
+      userId,
+      registerNumber,
+      brand,
+      year,
+      type,
+      fuelType,
+      seatCapacity,
+      InsuranceNumber,
+      InsuranceExpiry,
+      PermitExpiry,
+      status,
+    } = req.body;
+
+    const checkVehicle = await Vehicle.findOne({ _id: id });
+    if (!checkVehicle) {
+      return res.status(403).json({ message: "Vehicle not found" });
+    }
     
-}
+    const updateOne = await Vehicle.findByIdAndUpdate(
+      id,
+      {
+        user: userId,
+        registerNumber,
+        brand,
+        year,
+        type,
+        fuelType,
+        seatCapacity,
+        InsuranceNumber,
+        InsuranceExpiry,
+        PermitExpiry,
+        status,
+      },
+      { new: true },
+    );
+    res
+      .status(200)
+      .json({
+        message: "Vehicle Details Updated Successfully",
+        data: updateOne,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
 
-const getOneVehicle =async(req,res)=>{
-    try {
-        const {userId} = req.query;
-        const {id} =req.params;
-        const getOne = await Vehicle.findOne({_id:id,user:userId})
-        res.status(200).json({message:"One Vehicle Details",data:getOne})
-        
-    } catch (error) {
-         res.status(500).json({message:"Server Error Occured",error:error.message})
-    }
-}
-
-const updateVehicle = async(req,res)=>{
-    try {
-        const errors =validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({message:"All Feilds Are Required",
-                errors:errors.array().map((err)=>err.msg).join(', ')});
-        }
-        const {id} =req.params;
-        const {userId,registerNumber,brand,year,type,fuelType,seatCapacity,InsuranceNumber,InsuranceExpiry,
-            PermitExpiry,status
-        } =req.body;
-        const updateOne = await Vehicle.findByIdAndUpdate(
-            id,
-            {user:userId,registerNumber,brand,year,type,fuelType,seatCapacity,InsuranceNumber,InsuranceExpiry,
-            PermitExpiry,status},
-            {new:true}
-        );
-        res.status(200).json({message:"Vehicle Details Updated Successfully",data:updateOne})
-
-    } catch (error) {
-        res.status(500).json({message:"Server Error Occured",error:error.message})
-    }
-}
-
-const deleteVehicle = async(req,res)=>{
-    try {
-        const {id}=req.params;
-        const {userId} =req.query;
-        const deleteOne = await Vehicle.findByIdAndDelete({_id:id,user:userId});
-        res.status(200).json({message:"Vehicle Deleted Successfully"});
-    } catch (error) {
-         res.status(500).json({message:"Server Error Occured",error:error.message})
-    }
-}
-module.exports ={createVehicle,getAllVehicles,getOneVehicle,updateVehicle,deleteVehicle}
+const deleteVehicle = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.query;
+    const deleteOne = await Vehicle.findByIdAndDelete({
+      _id: id,
+      user: userId,
+    });
+    res.status(200).json({ message: "Vehicle Deleted Successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server Error Occured", error: error.message });
+  }
+};
+module.exports = {
+  createVehicle,
+  getAllVehicles,
+  getOneVehicle,
+  updateVehicle,
+  deleteVehicle,
+};
